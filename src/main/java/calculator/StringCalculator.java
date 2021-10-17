56db79dfd81fe85d8d8622bf403ab38b1b56f11e
komalpisar <pisar.komal4@gmail.com>
2021-10-17 22:33:08 +0530

    first and second cases committed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,11 +1,66 @@
 package calculator;
 
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 class StringCalculator {
+   
+    private String delimiter;
+	private String numbers;
 
-    public int add(String input) {
-        return 0;
-    }
+	private StringCalculator(String delimiter, String numbers) {
+		this.delimiter = delimiter;
+		this.numbers = numbers;
+	}
 
+	private int add() {
+		ensureNoNegativeNumbers();
+		return ((StringCalculator) getNumbers()).add();
+	}
+
+	private void ensureNoNegativeNumbers() {
+		String negativeNumberSequence = getNumbers().filter(n -> n < 0)
+				.mapToObj(Integer::toString)
+				.collect(Collectors.joining(","));
+		if (!negativeNumberSequence.isEmpty()) {
+			throw new IllegalArgumentException("negative number: " + negativeNumberSequence);
+		}
+	}
+
+	private IntStream getNumbers() {
+		if (numbers.isEmpty()) {
+			return IntStream.empty();
+		} else {
+			return Stream.of(numbers.split(delimiter))
+					.mapToInt(Integer::parseInt)
+					.map(n -> n % 1000);
+		}
+	}
+
+	public static int add(String input) {
+		return parseInput(input).add();
+	}
+	
+	private static StringCalculator parseInput(String input) {
+		if (input.startsWith("//")) {
+			String[] headerAndNumberSequence = input.split("\n", 2);
+			String delimiter = parseDelimiter(headerAndNumberSequence[0]);
+			return new StringCalculator(delimiter, headerAndNumberSequence[1]);
+		} else {
+			return new StringCalculator(",|\n", input);
+		}
+	}
+
+	private static String parseDelimiter(String header) {
+		String delimiter = header.substring(2);
+		if (delimiter.startsWith("[")) {
+			delimiter = delimiter.substring(1, delimiter.length() - 1);
+		}
+		return Stream.of(delimiter.split("]\\["))
+				.map(Pattern::quote)
+				.collect(Collectors.joining("|"));
+	}
 }
 
-test 
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,32 @@
 package calculator;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	//1. take 0,1,2 number and sum 
+		@Test
+		public void sumsEmptyStringTo0() {
+			assertThat(StringCalculator.add(""), is(0));
+		}
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+		@Test
+		public void sumsSingleNumberToItself() {
+			assertThat(StringCalculator.add("5"), is(5));
+			assertThat(StringCalculator.add("42"), is(42));
+		}
+
+		@Test
+		public void sumsTwoNumbersSeperatedByComma() {
+			assertThat(StringCalculator.add("1,2"), is(3));
+			assertThat(StringCalculator.add("1,3"), is(4));
+		}
+
+		
+		@Test
+		public void sumsThreeNumbersSeperatedByComma() {
+			assertThat(StringCalculator.add("1,2,3"), is(6));
+		}
 }
